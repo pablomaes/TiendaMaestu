7963c315b287f79c73fecd3443d38f2b61cbe6ad
pablomaes <pablomaestu32@gmail.com>
2021-11-01 18:22:24 -0300

    Desafío 2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar.js';
+
+function App() {
+  return  (
+    <div>
+    <NavBar/>
+    </div>
+    );
+}
+
+export default App;
